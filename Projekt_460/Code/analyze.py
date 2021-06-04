8da9f71748b466d686fd76c88901ab23eebfe468
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2021-06-04 21:35:06 +0200

    basic plot functions

--- Projekt_460/Code/analyze.py
+++ Projekt_460/Code/analyze.py
@@ -1,5 +1,25 @@
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 
+class Analyze():
+    
+    @staticmethod
+    def plot_results(style_sheet_path,results ,y, x = "time", x_label = None, y_label = None, title = None ):
+
+        plt.style.use(style_sheet_path) 
+        
+        fig, ax = plt.subplots()
+        
+        if title:
+            ax.set_title(title)
+        if x_label:
+            ax.set_xlabel(x_label)
+        if y_label:
+            ax.set_ylabel(y_label)
+        for var in y:
+            ax.plot(results[x], results[var])
+        
+        return ax
+
+    
