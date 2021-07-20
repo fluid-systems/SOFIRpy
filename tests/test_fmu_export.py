65bc30f9b30abba8cd06e31770f2e3dbc908376c
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2021-07-20 09:33:02 +0200

    new test

--- tests/test_fmu_export.py
+++ tests/test_fmu_export.py
@@ -16,7 +16,6 @@ def data_import():
 def test_datasheet_import(data_import):
 
     datasheet_dir = current_dir +r'/Gitlab/fair_sim_release/tests/test_data/datasheets'
-    print(datasheet_dir)
     datasheets = { 'component1' : 'test_datasheet_1'}                
     data_import.read_datasheet(datasheet_dir, datasheets)
     expected  = {'component1.use_N_in': 'true', 'component1.flowCharacteristic.c': '{-0.31462,0.36629,5.0907}', 'component1.p_a_start': 1}
@@ -41,5 +40,3 @@ def test_model_modifiers_setter(data_import, input = ['  redeclare  package  med
 
     data_import.model_modifiers = input
 
-def test_read_data_sheet(data_import):
-    pass
