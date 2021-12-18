7ad990c5cdfd9327a0a17558f21173d8bd6b30dc
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2021-12-18 16:22:55 +0100

    bug fix

--- fair_sim/__init__.py
+++ fair_sim/__init__.py
@@ -1,8 +1,8 @@
-from .fmu_export import export_fmu
-from .simulate import simulate
-from .project import Project
-from .read_hdf5_data import read_data, read_entire_group_content
-from .store_data import store_data, append_attributes
-from .store_input_arguments import store_input_arguments
-from .workflow import workflow
-from .plot import plot_results
+from fair_sim.fmu_export import export_fmu
+from fair_sim.simulate import simulate
+from fair_sim.project import Project
+from fair_sim.read_hdf5_data import read_data, read_entire_group_content
+from fair_sim.store_data import store_data, append_attributes
+from fair_sim.store_input_arguments import store_input_arguments
+from fair_sim.workflow import workflow
+from fair_sim.plot import plot_results
