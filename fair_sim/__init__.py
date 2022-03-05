f521bac32f47294b9b07887602c306ba6439a727
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-03-05 13:30:01 +0100

    hdf5 and project_dir added

--- fair_sim/__init__.py
+++ fair_sim/__init__.py
@@ -2,5 +2,7 @@ from .fmu_export.dymola_fmu_export import export_dymola_model
 from .fmu_export.open_modelica_fmu_export import export_open_modelica_model
 from .simulation.simulate import simulate, SimulationEntity
 from .project.project import Project
+from .project.hdf5 import HDF5
+from .project.project_dir import ProjectDir
 from .project.store_input_arguments import store_input_arguments
 from .project.plot import plot_results
