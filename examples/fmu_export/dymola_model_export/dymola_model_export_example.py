2a1e5a1962ef27cfc56a37d17aec860297cd1a93
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-03-05 13:29:11 +0100

    refactor

--- examples/fmu_export/dymola_model_export/dymola_model_export_example.py
+++ examples/fmu_export/dymola_model_export/dymola_model_export_example.py
@@ -6,4 +6,4 @@ model_path = dir_path.parent / "DC_Motor.mo"
 output_direcotry = dir_path
 dymola_exe_path = r"C:\Program Files\Dymola 2018 FD01\bin64\Dymola.exe"
 
-success = export_dymola_model(dymola_exe_path, model_path, output_direcotry)
+export_dymola_model(dymola_exe_path, model_path, output_direcotry)
--- examples/fmu_export/open_modelica_model_export/open_modelica_model_export_example.py
+++ examples/fmu_export/open_modelica_model_export/open_modelica_model_export_example.py
@@ -5,4 +5,4 @@ dir_path = Path(__file__).parent.parent
 model_path = dir_path / "DC_Motor.mo"
 output_direcotry = dir_path
 
-success = export_open_modelica_model(model_path, dir_path)
+export_open_modelica_model(model_path, dir_path)
