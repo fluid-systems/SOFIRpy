cb68ab77307aebebc3fe017b2af73e86708a77b3
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-03-10 20:52:10 +0100

    refactor

--- fair_sim/__init__.py
+++ fair_sim/__init__.py
@@ -1,6 +1,7 @@
 from .fmu_export.dymola_fmu_export import export_dymola_model
 from .fmu_export.open_modelica_fmu_export import export_open_modelica_model
-from .simulation.simulation import simulate, SimulationEntity
+from .simulation.simulation import simulate
+from .simulation.simulation_entity import SimulationEntity
 from .project.project import Project
 from .project.hdf5 import HDF5
 from .project.project_dir import ProjectDir
