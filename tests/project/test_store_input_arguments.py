c8f027d9e47102f8203a36afdfe45096f6dc0e6d
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-05-16 09:40:04 +0200

    added test for store_input_arguments.py

---
+++ tests/project/test_store_input_arguments.py
@@ -0,0 +1,48 @@
+import sys
+import os
+from pathlib import Path
+import pytest
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../'))
+from sofirpy import store_input_arguments
+
+# %%
+
+class TestClass:
+
+    @store_input_arguments
+    def __init__(self, a, b, c = 1, d = 2) -> None:...
+
+    @store_input_arguments
+    def methode1(self) -> None:...
+
+    @store_input_arguments
+    def methode2(self, a = 1) -> None:...
+
+    @store_input_arguments
+    def methode3(self, a) -> None:...
+
+
+def test_store_input_arguments() -> None:
+
+    test_class = TestClass(1, b = 5, c = 4)
+    test_class.methode1()
+    test_class.methode2(a = None)
+    test_class.methode3(list())
+    test_class.methode3(dict())
+
+    assert test_class.__input_arguments__ == {
+        "__init__": {
+            "a": 1,
+            "b": 5,
+            "c": 4,
+            "d": 2
+        },
+        "methode1": {},
+        "methode2": {
+            "a": None
+        },
+        "methode3": {
+            "a": {}
+        }
+    }
