f4c4b4ac55b1845ba856ce6f7dd8ab7e264e04a6
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-07-04 11:23:01 +0200

    input validation for simulate function

--- sofirpy/simulation/simulation.py
+++ sofirpy/simulation/simulation.py
@@ -2,13 +2,15 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
-from typing import Optional, Union
+from typing import Any, Optional, Union
 import numpy as np
 import pandas as pd
 from alive_progress import alive_bar
 from sofirpy.simulation.simulation_entity import SimulationEntity
 from sofirpy.simulation.fmu import Fmu
+import sofirpy.utils as utils
 
 
 @dataclass(frozen=True)
@@ -30,15 +32,17 @@ class SystemParameter:
 
     Args:
             system (System): System object
-            name (str): name of the paremeter
+            name (str): name of the parameter
     """
 
     system: System
     name: str
 
+
 @dataclass(frozen=True)
 class LoggedParameter(SystemParameter):
     """LoggedParameter object representing a parameter in a system that is logged."""
+
 
 @dataclass(frozen=True)
 class ConnectionPoint(SystemParameter):
@@ -97,7 +101,7 @@ class Simulation:
                 Defaults to 0.0.
 
         Returns:
-            pd.DataFrame: results dataframe with times series of logged
+            pd.DataFrame: result DataFrame with times series of logged
             parameters
         """
         time_series = np.arange(start_time, stop_time + step_size, step_size)
@@ -159,14 +163,14 @@ class Simulation:
     def create_result_df(
         self, parameters_to_log: list[LoggedParameter]
     ) -> pd.DataFrame:
-        """Initialise the result dataframe. By default the first column contains the time.
+        """Initialise the result dataFrame. By default the first column contains the time.
 
         Args:
             parameters_to_log (list[LoggedParameter]): list of parameters that
                 should be logged
 
         Returns:
-            pd.DataFrame: dataframe with only the column names
+            pd.DataFrame: dataFrame with only the column names
         """
         columns = [
             f"{parameter.system.name}.{parameter.name}"
@@ -194,8 +198,10 @@ class Simulation:
 
         return units
 
-ConnectionsInfo = list[dict[str,str]]
+
+ConnectionsInfo = list[dict[str, str]]
 SystemInfo = list[dict[str, Union[str, ConnectionsInfo]]]
+
 
 def simulate(
     stop_time: Union[float, int],
@@ -216,7 +222,7 @@ def simulate(
         step_size (float): step size for the simulation
         fmu_infos (Optional[SystemInfo], optional):
             Defines which fmus should be simulated and how they are connected
-            to other systems. It needs to have the following formart:
+            to other systems. It needs to have the following format:
 
             >>> fmu_infos = [
             ... {"name": "<name of the fmu>",
@@ -316,36 +322,14 @@ def simulate(
 
     Returns:
         Union[pd.DataFrame, tuple[pd.DataFrame, dict[str,str]]]:
-            Result Dataframe with times series of logged parameters, units of
+            Result DataFrame with times series of logged parameters, units of
             logged parameters.
     """
-    if not isinstance(stop_time, (float, int)):
-        raise TypeError(f"'stop_time' is {type(stop_time)}; expected float, int")
+
+    _validate_input(stop_time, step_size, fmu_infos, model_infos, model_classes)
+
     stop_time = float(stop_time)
-
-    if not isinstance(step_size, (float, int)):
-        raise TypeError(f"'step_size' is {type(step_size)}; expected float, int")
     step_size = float(step_size)
-
-    if fmu_infos and not isinstance(fmu_infos, list):
-        raise TypeError(f"'fmu_infos' is {type(fmu_infos)}; expected list")
-
-    if model_infos and not isinstance(model_infos, list):
-        raise TypeError(f"'model_infos' is {type(model_infos)}; expected list")
-
-    if not fmu_infos and not model_infos:
-        raise ValueError(
-            "'fmu_infos' and 'model_infos' are empty; expected al least one to be not empty"
-        )
-
-    if model_classes and not isinstance(model_classes, dict):
-        raise TypeError(f"'model_classes' is {type(model_classes)}; expected dict")
-
-    if stop_time <= 0:
-        raise ValueError(f"stop_time is {stop_time}; expected > 0")
-
-    if step_size <= 0 or step_size >= stop_time:
-        raise ValueError(f"'step_size' is {step_size}; expected (0, {stop_time})")
 
     if fmu_infos is None:
         fmu_infos = []
@@ -369,6 +353,15 @@ def simulate(
     return results
 
 
+class SystemInfoKeys(Enum):
+    SYSTEM_NAME = "name"
+    FMU_PATH = "path"
+    CONNECTIONS = "connections"
+    INPUT_PARAMETER = "parameter name"
+    CONNECTED_SYSTEM = "connect to system"
+    OUTPUT_PARAMETER = "connect to external parameter"
+
+
 def init_systems(
     fmu_infos: SystemInfo,
     model_infos: SystemInfo,
@@ -390,14 +383,13 @@ def init_systems(
         step_size (float): step size of the simulation
 
     Returns:
-        dict[str, System]: Dictrionary with system names as keys and the
+        dict[str, System]: Dictionary with system names as keys and the
             corresponding System instance as values.
     """
     systems = {}
     for fmu_info in fmu_infos:
-        fmu_name: str = fmu_info["name"]
-        _fmu_path: str = fmu_info["path"]
-        fmu_path = Path(_fmu_path)
+        fmu_name: str = fmu_info[SystemInfoKeys.SYSTEM_NAME.value]
+        fmu_path: Path = utils.convert_str_to_path(fmu[SystemInfoKeys.FMU_PATH.value])
         fmu = Fmu(fmu_path, step_size)
         fmu.initialize_fmu()
         system = System(fmu, fmu_name)
@@ -405,7 +397,7 @@ def init_systems(
         print(f"FMU '{fmu_name}' initialized.")
 
     for model_info in model_infos:
-        model_name: str = model_info["name"]
+        model_name: str = model_info[SystemInfoKeys.SYSTEM_NAME.value]
         py_model = model_classes[model_name]
         system = System(py_model, model_name)
         systems[model_name] = system
@@ -423,27 +415,27 @@ def init_connections(
     Args:
         system_infos (SystemInfo):
             Defines how all systems are connected.
-        systems (dict[str, System]): Dictrionary with system names as keys and
+        systems (dict[str, System]): Dictionary with system names as keys and
             the corresponding System instance as values.
 
     Returns:
         list[Connection]: List of Connections.
     """
-    all_connections = []
+    all_connections: list[Connection] = []
 
     for system_info in system_infos:
-        if system_info.get("connections"):
-            connections = system_info["connections"]
-            this_system_name = system_info["name"]
+        if system_info.get(SystemInfoKeys.CONNECTIONS.value) is not None:
+            connections = system_info[SystemInfoKeys.CONNECTIONS.value]
+            this_system_name = system_info[SystemInfoKeys.SYSTEM_NAME.value]
             this_system = systems[this_system_name]
             for con in connections:
-                this_parameter_name = con["parameter name"]
+                this_parameter_name = con[SystemInfoKeys.INPUT_PARAMETER.value]
                 this_connection_point = ConnectionPoint(
                     this_system, this_parameter_name
                 )
-                other_system_name = con["connect to system"]
+                other_system_name = con[SystemInfoKeys.CONNECTED_SYSTEM.value]
                 other_system = systems[other_system_name]
-                other_parameter_name = con["connect to external parameter"]
+                other_parameter_name = con[SystemInfoKeys.OUTPUT_PARAMETER.value]
                 other_connection_point = ConnectionPoint(
                     other_system, other_parameter_name
                 )
@@ -454,14 +446,14 @@ def init_connections(
 
 
 def init_parameter_list(
-    parameters_to_log: Optional[dict[str, list[str]]], systems: dict[str, System]
+    parameters_to_log: dict[str, list[str]], systems: dict[str, System]
 ) -> list[LoggedParameter]:
     """Initialize all parameters that should be logged.
 
     Args:
-        parameters_to_log (Optional[dict[str, list[str]]]): Defines which
-            paramters should be logged.
-        systems (dict[str, System]): Dictrionary with system names as keys and
+        parameters_to_log (dict[str, list[str]]): Defines which
+            parameters should be logged.
+        systems (dict[str, System]): Dictionary with system names as keys and
             the corresponding System instance as values.
 
     Returns:
@@ -479,3 +471,132 @@ def init_parameter_list(
             log.append(parameter_to_log)
 
     return log
+
+
+def _validate_input(
+    stop_time: Union[float, int],
+    step_size: float,
+    fmu_infos: Optional[SystemInfo] = None,
+    model_infos: Optional[SystemInfo] = None,
+    model_classes: Optional[dict[str, SimulationEntity]] = None,
+) -> None:
+
+    if not isinstance(stop_time, (float, int)):
+        raise TypeError(f"'stop_time' is {type(stop_time)}; expected float, int")
+
+    if not isinstance(step_size, (float, int)):
+        raise TypeError(f"'step_size' is {type(step_size)}; expected float, int")
+
+    if stop_time <= 0:
+        raise ValueError(f"stop_time is {stop_time}; expected > 0")
+
+    if step_size <= 0 or step_size >= stop_time:
+        raise ValueError(f"'step_size' is {step_size}; expected (0, {stop_time})")
+
+    fmu_names = _validate_fmu_infos(fmu_infos)
+    model_names = _validate_model_infos(model_infos)
+    all_system_names = [*fmu_names, *model_names]
+
+    if len(set(all_system_names)) < len(all_system_names):
+        raise ValueError(
+            f"Duplicate names in system infos."
+        )
+
+    if not fmu_infos and not model_infos:
+        raise ValueError(
+            "'fmu_infos' and 'model_infos' are empty; expected al least one to be not empty"
+        )
+
+    _validate_model_classes(model_classes, model_names)
+
+
+
+
+def _validate_fmu_infos(fmu_infos: SystemInfo) -> list[str]:
+
+    if fmu_infos is None:
+        return
+
+    if not isinstance(fmu_infos, list):
+        raise TypeError(f"'fmu_infos' is {type(fmu_infos)}; expected list")
+
+    fmu_names: list[str] = []
+
+    for fmu_info in fmu_infos:
+        _check_key_exists(SystemInfoKeys.SYSTEM_NAME.value, fmu_info)
+        _check_value_type(
+            SystemInfoKeys.SYSTEM_NAME.value, fmu_info[SystemInfoKeys.SYSTEM_NAME.value], str
+        )
+        fmu_names.append(fmu_info[SystemInfoKeys.SYSTEM_NAME.value])
+        _check_key_exists(SystemInfoKeys.FMU_PATH.value, fmu_info)
+        _check_key_exists(
+            SystemInfoKeys.FMU_PATH.value, fmu_info[SystemInfoKeys.FMU_PATH.value], (str, Path)
+        )
+        if fmu_info.get(SystemInfoKeys.CONNECTIONS.value) is not None:
+            _validate_connection_infos(fmu_info[SystemInfoKeys.CONNECTIONS.value])
+
+    return fmu_names
+
+
+def _validate_model_infos(model_infos: SystemInfo) -> list[str]:
+
+    if model_infos is None:
+        return
+
+    if model_infos and not isinstance(model_infos, list):
+        raise TypeError(f"'model_infos' is {type(model_infos)}; expected list")
+
+    model_names: list[str] = []
+
+    for model_info in model_infos:
+        _check_key_exists(SystemInfoKeys.SYSTEM_NAME.value, model_info)
+        _check_value_type(
+            SystemInfoKeys.SYSTEM_NAME.value, model_info[SystemInfoKeys.SYSTEM_NAME.value], str
+        )
+        model_names.append(model_info[SystemInfoKeys.SYSTEM_NAME.value])
+        if model_info.get(SystemInfoKeys.CONNECTIONS.value) is not None:
+            _validate_connection_infos(model_info[SystemInfoKeys.CONNECTIONS.value])
+
+    return model_names
+
+
+def _validate_connection_infos(connection_infos: ConnectionsInfo) -> None:
+
+    if not isinstance(connection_infos, list):
+        raise TypeError(
+            f"value of key '{SystemInfoKeys.CONNECTIONS.value}' is {type(connection_infos)}; expected list"
+        )
+    for connection in connection_infos:
+        for key in (
+            SystemInfoKeys.INPUT_PARAMETER.value,
+            SystemInfoKeys.OUTPUT_PARAMETER.value,
+            SystemInfoKeys.SYSTEM_NAME.value,
+        ):
+            _check_key_exists(key, connection)
+            _check_value_type(key, connection[key], str)
+
+
+def _check_key_exists(key: str, info_dict: dict) -> None:
+    if key not in info_dict:
+        raise KeyError(f"missing key '{key}' in {info_dict}")
+
+
+def _check_value_type(key: str, value: Any, _type: Any) -> None:
+    if not isinstance(value, _type):
+        type_name = _type.__name__
+        if isinstance(_type, tuple):  # if multiple types allowed
+            type_name = ", ".join([t.name for t in _type])
+        raise TypeError(f"value of key '{key}' is {type(value)}; expected {type_name}")
+
+def _validate_model_classes(model_classes: Union[dict[str, SimulationEntity], None], model_names: list[str]):
+
+        if model_classes is None:
+            if model_names:
+                raise ValueError("Models are defined but 'model_classes' is 'None'")
+            return
+
+        if not isinstance(model_classes, dict):
+            raise TypeError(f"'model_classes' is {type(model_classes)}; expected dict")
+
+        #TODO check if names in models classes are defined in model names
+# %%
