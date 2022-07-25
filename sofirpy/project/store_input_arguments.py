233d6a4bc3519ffd869fe074fb99152dd137b1ee
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-07-25 12:59:11 +0200

    fixed typos

--- sofirpy/project/store_input_arguments.py
+++ sofirpy/project/store_input_arguments.py
@@ -2,32 +2,31 @@ from inspect import getfullargspec
 from functools import wraps
 from typing import Callable
 
-from attr import has
 
 def store_input_arguments(func: Callable):
     """Decorator that lets you store the input arguments of class methods.
 
-    The input arguments will be stored in a nested dictrionay with the following structure:
-    
-    >>> {"<methode_name1>": 
+    The input arguments will be stored in a nested dictionary with the following structure:
+
+    >>> {"<method_name1>":
     ...     {
     ...         "<input_name1>": <input_value1>,
     ...         "<input_name2>": <input_value2>
     ...     },
-    ...  "<methode_name2>": 
+    ...  "<method_name2>":
     ...     {
     ...         "<input_name1>": <input_value1>,
     ...         "<input_name2>": <input_value2>
     ... }
     ... }
-    
-    The dictrionay will be stored as a class attribute with the name '__input_arguments__'.
+
+    The dictionary will be stored as a class attribute with the name '__input_arguments__'.
 
     Args:
-        func (Callable): Methode to decorate
+        func (Callable): Method to decorate
 
     Returns:
-        Callable: Decorated Methode
+        Callable: Decorated Method
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -42,9 +41,9 @@ def store_input_arguments(func: Callable):
                 if name not in list(inputs.keys()):
                     inputs[name] = val
         func(*args, **kwargs)
-        methode_input_info = {func.__name__: inputs}
+        method_input_info = {func.__name__: inputs}
         if hasattr(args[0], "__input_arguments__"):
-            args[0].__input_arguments__ = {**args[0].__input_arguments__, **methode_input_info}
+            args[0].__input_arguments__ = {**args[0].__input_arguments__, **method_input_info}
         else:
-            args[0].__input_arguments__ = methode_input_info
+            args[0].__input_arguments__ = method_input_info
     return wrapper
