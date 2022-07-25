f5df4ff8cc88ba0722362e29fc049ffbc3acbef4
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-07-25 12:51:34 +0200

    test refactor and removed sys.path.append

--- tests/project/test_store_input_arguments.py
+++ tests/project/test_store_input_arguments.py
@@ -1,45 +1,38 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__),'../../'))
+import pytest
 from sofirpy import store_input_arguments
 
 
-class TestClass:
+class DummyClass:
+    @store_input_arguments
+    def __init__(self, a, b, c=1, d=2) -> None:
+        ...
 
     @store_input_arguments
-    def __init__(self, a, b, c = 1, d = 2) -> None:...
+    def methode1(self) -> None:
+        ...
 
     @store_input_arguments
-    def methode1(self) -> None:...
+    def methode2(self, a=1) -> None:
+        ...
 
     @store_input_arguments
-    def methode2(self, a = 1) -> None:...
+    def methode3(self, a) -> None:
+        ...
 
-    @store_input_arguments
-    def methode3(self, a) -> None:...
+@pytest.fixture
+def test_class() -> DummyClass:
+    return DummyClass(1, b=5, c=4)
 
+def test_store_input_arguments(test_class: DummyClass) -> None:
 
-def test_store_input_arguments() -> None:
-
-    test_class = TestClass(1, b = 5, c = 4)
     test_class.methode1()
-    test_class.methode2(a = None)
+    test_class.methode2(a=None)
     test_class.methode3(list())
     test_class.methode3(dict())
 
     assert test_class.__input_arguments__ == {
-        "__init__": {
-            "a": 1,
-            "b": 5,
-            "c": 4,
-            "d": 2
-        },
+        "__init__": {"a": 1, "b": 5, "c": 4, "d": 2},
         "methode1": {},
-        "methode2": {
-            "a": None
-        },
-        "methode3": {
-            "a": {}
-        }
+        "methode2": {"a": None},
+        "methode3": {"a": {}},
     }
