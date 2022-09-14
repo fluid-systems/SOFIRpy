c9d6ae8f4cc2ca6be149a8ba9a23fad1d3d293ac
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2022-09-14 13:51:38 +0200

    make version dynamic

--- src/sofirpy/__init__.py
+++ src/sofirpy/__init__.py
@@ -1,12 +1,8 @@
-from .fmu_export.dymola_fmu_export import export_dymola_model
-from .fmu_export.open_modelica_fmu_export import export_open_modelica_model
-from .simulation.simulation import simulate
-from .simulation.simulation_entity import SimulationEntity
-from .project.project import Project
-from .project.hdf5 import HDF5
-from .project.project_dir import ProjectDir
-from .project.store_input_arguments import store_input_arguments
-from .simulation.plot import plot_results
+"""Framework for simulating FMUs and custom models written in python."""
+
+__author__ = 'Daniele Inturri'
+__email__ = 'daniele.inturri@stud.tu-darmstadt.de'
+__version__ = "0.1.0"
 
 __all__ = [
     "export_dymola_model",
@@ -19,3 +15,13 @@ __all__ = [
     "store_input_arguments",
     "plot_results"
 ]
+
+from .fmu_export.dymola_fmu_export import export_dymola_model
+from .fmu_export.open_modelica_fmu_export import export_open_modelica_model
+from .project.hdf5 import HDF5
+from .project.project import Project
+from .project.project_dir import ProjectDir
+from .project.store_input_arguments import store_input_arguments
+from .simulation.plot import plot_results
+from .simulation.simulation import simulate
+from .simulation.simulation_entity import SimulationEntity
