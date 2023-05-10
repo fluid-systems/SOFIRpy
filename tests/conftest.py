a07d9ba79f61670ae881eb5d181503ed7eb5e485
Daniele <daniele.inturri@stud.tu-darmstadt.de>
2023-05-10 17:56:14 +0200

    add conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from typing import Union
 
 import pytest
 
-from sofirpy.simulation.simulation import FmuPaths, ModelInstances
+from sofirpy.simulation.simulation import FmuPaths, ModelInstances, StartValues
 from sofirpy.simulation.simulation_entity import SimulationEntity
 
 
@@ -23,38 +23,30 @@ def fmu_paths() -> FmuPaths:
 class PID(SimulationEntity):
     """Simple implementation of a discrete pid controller"""
 
-    def __init__(
-        self, step_size, K_p=1, K_i=0, K_d=0, set_point=0, u_max=1000, u_min=-1000
-    ) -> None:
-
-        self.T_a = step_size
-        self.K_p = K_p
-        self.K_i = K_i
-        self.K_d = K_d
-        self.set_point = set_point
+    def __init__(self):
+        self.parameters = {
+            "step_size": 1e-3,
+            "K_p": 1,
+            "K_i": 0,
+            "K_d": 0,
+            "set_point": 0,
+            "u_max": 1000,
+            "u_min": -1000,
+        }  # TODO change step_size to Abtastrate
         self.inputs = {"speed": 0}
         self.outputs = {"u": 0}
-        self.d_0 = K_p * (
-            1 + (self.T_a * self.K_i / self.K_p) + self.K_d / (self.K_p * self.T_a)
-        )
-        self.d_1 = K_p * (-1 - 2 * self.K_d / (self.K_p * self.T_a))
-        self.d_2 = K_p * self.K_d / (self.K_p * self.T_a)
-        self.error = [0, 0, 0]
-        self.u_max = u_max
-        self.u_min = u_min
 
-    def compute_error(self) -> None:
+    def compute_error(self):
 
         self.error[2] = self.error[1]
         self.error[1] = self.error[0]
-        self.error[0] = self.set_point - self.inputs["speed"]
+        self.error[0] = self.parameters["set_point"] - self.inputs["speed"]
 
-    def set_parameter(self, parameter_name, parameter_value) -> None:
+    def set_input(self, input_name, input_value):
 
-        self.inputs[parameter_name] = parameter_value
+        self.parameters[input_name] = input_value
 
-    def do_step(self, _) -> None:
-
+    def do_step(self, _):
         self.compute_error()
         u = (
             self.outputs["u"]
@@ -70,10 +62,45 @@ class PID(SimulationEntity):
 
         self.outputs["u"] = u
 
-    def get_parameter_value(self, output_name) -> Union[int, float]:
+    def set_parameter(self, parameter_name, parameter_value) -> None:
+        self.inputs[parameter_name] = parameter_value
+
+    def get_parameter_value(self, output_name):
         return self.outputs[output_name]
+
+    def initialize(self, start_values) -> None:
+        self.apply_start_values(start_values)
+        K_p = self.parameters["K_p"]
+        K_i = self.parameters["K_i"]
+        K_d = self.parameters["K_d"]
+        T_a = self.parameters["step_size"]
+        self.d_0 = K_p * (1 + (T_a * K_i / K_p) + K_d / (K_p * T_a))
+        self.d_1 = K_p * (-1 - 2 * K_d / (K_p * T_a))
+        self.d_2 = K_p * K_d / (K_p * T_a)
+        self.error = [0, 0, 0]
+        self.u_max = self.parameters["u_max"]
+        self.u_min = self.parameters["u_min"]
+
+    def apply_start_values(self, start_values: StartValues) -> None:
+        for name, value in start_values.items():
+            self.parameters[name] = value
+
+
+@pytest.fixture
+def start_values() -> StartValues:
+    return {
+        "pid": {
+            "step_size": 1e-3,
+            "K_p": 3,
+            "K_i": 20,
+            "K_d": 0.1,
+            "set_point": 100,
+            "u_max": 100,
+            "u_min": 0,
+        }
+    }
 
 
 @pytest.fixture
 def model_instances() -> ModelInstances:
-    return {"pid": PID(1e-3, 3, 20, 0.1, set_point=100, u_max=100, u_min=0)}
+    return {"pid": PID()}
